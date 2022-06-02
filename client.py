0f1e7500e244587546add72d4885ae193ac20b58
Vitor Rosa <vitorosa306@gmail.com>
2022-06-01 22:20:51 -0300

    Adding Error Handlers in code

--- client.py
+++ client.py
@@ -2,8 +2,22 @@ import socket
 from _thread import *
 
 serverPort = 8080
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientSocket.connect(('localhost', serverPort))
+
+try:
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as e:
+    print("Create socket failed: %s" % e)
+    sys.exit(1)
+
+try:
+    clientSocket.connect(('localhost', serverPort))
+except socket.gaierror as e:
+    print("Address-related error: %s" % e)
+    sys.exit(1)
+except socket.error as e:
+    print("Error connecting to server: %s" % e)
+    sys.exit(1)
+
 print("Server Connection Accepted")
 
 user = input("Insert username: ")
@@ -11,13 +25,19 @@ user = input("Insert username: ")
 def receiveMessage(connection):
     while True:
         sentence = input()
-        connection.send((f'<{user}> {sentence}').encode('utf-8'))
+        try:
+            connection.send((f'<{user}> {sentence}').encode('utf-8'))
+        except socket.error as e:
+            print("Error sending data: %s" % e)
 
 def sendMessage(connection):
     while True:
-        serverSentence = connection.recv(2048)
-        serverSentence = serverSentence.decode('utf-8')
-        print(serverSentence)
+        try:
+            serverSentence = connection.recv(2048)
+            serverSentence = serverSentence.decode('utf-8')
+            print(serverSentence)
+        except socket.error as e:
+            print("Error receiving data: %s" % e)
 
 start_new_thread(sendMessage, (clientSocket, ))
 start_new_thread(receiveMessage, (clientSocket, ))
--- server.py
+++ server.py
@@ -1,33 +1,49 @@
 import socket
 from _thread import *
+import sys
 
 serverPort = 8080
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as e:
+    print("Create socket failed: %s" % e)
+    sys.exit(1)
 
 clients = []
 
 try:
     serverSocket.bind(('localhost', serverPort))
 except socket.error as e:
-    print(str(e))
+    print("Error binding socket to the port: %s" %e )
+    sys.exit(1)
 
 print('Waiting for connection ...')
 serverSocket.listen()
 
 def threaded_client(connection):
     while True:
-        data = connection.recv(2048)
+        try:
+            data = connection.recv(2048)
+        except socket.error as e:
+            print("Error receiving data: %s" % e)
+            break
         if not data:
             break
         for client in clients:
             if client != connection:
-                client.send(data)
+                try:
+                    client.send(data)
+                except socket.error as e:
+                    print("Error sending data: %s" % e)
     connection.close()
 
 while True:
-    connectionSocket, addr = serverSocket.accept()
-    print('Conection established with: ' + addr[0] + ':' + str(addr[1]))
-    start_new_thread(threaded_client, (connectionSocket, ))
-    clients.append(connectionSocket)
+    try:
+        connectionSocket, addr = serverSocket.accept()
+        print('Conection established with: ' + addr[0] + ':' + str(addr[1]))
+        start_new_thread(threaded_client, (connectionSocket, ))
+        clients.append(connectionSocket)
+    except socket.error as e:
+        print("Error Establishing connection: %s" % e)
 
 connectionSocket.close()
